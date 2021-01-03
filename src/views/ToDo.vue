0784b53ac027eb1ef3db0651bdce06294f5c503e
waiscodes <b.wais@outlook.com>
2021-01-02 22:15:48 -0700

    added a for loop to map the items in the list

--- src/views/ToDo.vue
+++ src/views/ToDo.vue
@@ -6,9 +6,31 @@
       <input type="text" name="item" id="item" />
       <input type="submit" value="Add" />
     </form>
-    <ul class="todolist"></ul>
+    <ul class="todolist">
+      <li v-for="(item, index) in todos" :key="index">
+        {{ item.text }}
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+import { defineComponent, reactive } from "vue";
+
+export default defineComponent({
+  setup() {
+    const todos = reactive([
+      {
+        text: "First To Do Item",
+      },
+    ]);
+
+    return {
+      todos,
+    };
+  },
+});
+</script>
 
 <style scoped>
 .ToDo {
