1fea5502ade0d769023c59bc08d466d06ad57c5b
waiscodes <b.wais@outlook.com>
2021-01-02 21:22:09 -0700

    added to do list form

--- src/views/ToDo.vue
+++ src/views/ToDo.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="ToDo">
     <h2>To Do List</h2>
+    <form action="#">
+      <label for="item">Item</label>
+      <input type="text" name="item" id="item" />
+      <input type="submit" value="Add" />
+    </form>
   </div>
 </template>
 
